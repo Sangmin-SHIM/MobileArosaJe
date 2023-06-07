9456e4a8c39845c453b0e096e25c0279f51260bd
Julien Flusin <julienflusin3@gmail.com>
2023-06-07 13:54:33 +0200

    last fix, do not touch it works !!

--- app/src/main/java/com/example/arosaje/MainActivity.kt
+++ app/src/main/java/com/example/arosaje/MainActivity.kt
@@ -1,159 +1,125 @@
 package com.example.arosaje
 
-import android.app.Activity
-import android.content.ContentValues.TAG
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import android.util.Log
-import android.view.KeyEvent
 import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import java.io.File
-import java.io.IOException
-import java.text.SimpleDateFormat
-import java.util.Date
-import android.Manifest
-
+import androidx.activity.result.contract.ActivityResultContracts
 class MainActivity : AppCompatActivity() {
+
     private var webView: WebView? = null
-    private var mUploadMessage: ValueCallback<Array<Uri>>? = null
+    private var fileUploadCallback: ValueCallback<Array<Uri>>? = null
     private val FILECHOOSER_RESULTCODE = 1
-    val CAMERA_REQUEST_CODE = 101
-    private var mCameraPhotoPath: String? = null
 
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Crée un nom de fichier image unique
-        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val imageFileName = "JPEG_" + timeStamp + "_"
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val image = File.createTempFile(
-            imageFileName,  /* prefix */
-            ".jpg",         /* suffix */
-            storageDir      /* directory */
-        )
-        // Sauvegarde un fichier: chemin pour l'utilisation avec ACTION_VIEW intents
-        mCameraPhotoPath = image.absolutePath
-        return image
-    }
-
+    private val fileChooserLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                val data: Intent? = result.data
+                val resultArray = if (data != null) {
+                    val uri = data.data
+                    if (uri != null) {
+                        arrayOf(uri)
+                    } else {
+                        emptyArray()
+                    }
+                } else {
+                    emptyArray()
+                }
+                fileUploadCallback?.onReceiveValue(resultArray)
+                fileUploadCallback = null
+            } else {
+                fileUploadCallback?.onReceiveValue(null)
+                fileUploadCallback = null
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        webView = findViewById<WebView>(R.id.webview)
-        val url = getString(R.string.URL_LOGIN)
-        webView?.settings?.javaScriptEnabled = true
+
+        // ┌─────────────────────────────┐
+        // │          WebView            │
+        // └─────────────────────────────┘
+        var webView: WebView = findViewById<WebView>(R.id.webview)
+        val url_home = getString(R.string.URL_HOME)
+
+        webView.settings.javaScriptEnabled = true
         webView?.clearCache(true)
-        webView?.loadUrl(url)
-        webView?.webChromeClient = object : WebChromeClient() {
-            // For Android 5.0+
+        webView.loadUrl(url_home)
+        webView.webChromeClient = object : WebChromeClient() {
             override fun onShowFileChooser(
-                webView: WebView, filePathCallback: ValueCallback<Array<Uri>>,
+                webView: WebView,
+                filePathCallback: ValueCallback<Array<Uri>>,
                 fileChooserParams: FileChooserParams
             ): Boolean {
-                if (ContextCompat.checkSelfPermission(this@MainActivity, Manifest.permission.CAMERA)
-                    != PackageManager.PERMISSION_GRANTED) {
-                    // Permission is not granted, so request it
-                    ActivityCompat.requestPermissions(this@MainActivity, arrayOf(Manifest.permission.CAMERA), 101)
-                }
-                mUploadMessage = filePathCallback
-                var takePictureIntent: Intent? = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-                if (takePictureIntent?.resolveActivity(packageManager) != null) {
-                    var photoFile: File? = null
-                    try {
-                        photoFile = createImageFile()
-                        takePictureIntent.putExtra("PhotoPath", mCameraPhotoPath)
-                    } catch (ex: IOException) {
-                        Log.e(TAG, "Unable to create Image File", ex)
-                    }
+                fileUploadCallback = filePathCallback
 
-                    if (photoFile != null) {
-                        mCameraPhotoPath = Uri.fromFile(photoFile).toString()
-                        takePictureIntent.putExtra(
-                            MediaStore.EXTRA_OUTPUT,
-                            Uri.fromFile(photoFile)
-                        )
-                    } else {
-                        takePictureIntent = null
-                    }
+                openFileChooser()
 
-                }
-
-                val contentSelectionIntent = Intent(Intent.ACTION_GET_CONTENT)
-                contentSelectionIntent.addCategory(Intent.CATEGORY_OPENABLE)
-                contentSelectionIntent.type = "image/*"
-                val intentArray: Array<Intent?>
-                intentArray = takePictureIntent?.let { arrayOf(it) } ?: arrayOfNulls(0)
-                val chooserIntent = Intent(Intent.ACTION_CHOOSER)
-                chooserIntent.putExtra(Intent.EXTRA_INTENT, contentSelectionIntent)
-                chooserIntent.putExtra(Intent.EXTRA_TITLE, "Image Chooser")
-                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, intentArray)
-                startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE)
                 return true
             }
         }
 
-        webView?.webViewClient = object : WebViewClient() {
+        webView.webViewClient = object : WebViewClient(){
             override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
-                webView?.settings?.userAgentString = "app_mobile_v1"
-                webView?.loadUrl(url.toString())
+                if (url != null) {
+                    if (url.startsWith("tel:")) {
+                        val tel = Intent (Intent.ACTION_DIAL, Uri.parse(url));
+                        startActivity(tel);
+                        return true;
+                    } else if(url.contains("mailto:")) {
+                        if (view != null) {
+                            view.getContext().startActivity(
+                                Intent(Intent.ACTION_VIEW, Uri.parse(url)))
+                        };
+                        return true;
+                    } else {
+                        if (view != null) {
+                            view.loadUrl(url)
+                        };
+                        return true;
+                    }
+                }
+                webView.settings.userAgentString="app_mobile_v1"
+                webView.loadUrl(url.toString());
+
                 return true
             }
         }
     }
 
-    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView?.canGoBack() == true) {
-            webView?.goBack()
-            return true
-        }
-        return super.onKeyDown(keyCode, event)
+    private fun openFileChooser() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "image/*"
+
+        val chooserIntent = Intent.createChooser(intent, "Select File")
+        startActivityForResult(chooserIntent, FILECHOOSER_RESULTCODE)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
-        super.onActivityResult(requestCode, resultCode, intent)
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == FILECHOOSER_RESULTCODE) {
-            if (mUploadMessage == null) {
-                return // Si uploadMessage est null, on ne fait rien
+            if (fileUploadCallback == null) {
+                return
             }
-
-            var results: Array<Uri>? = null
-            // Vérifie si la réponse contient une data
-            if (resultCode == Activity.RESULT_OK) {
-                if (intent != null) {
-                    // Si l'intention n'est pas nulle, récupère l'Uri de la photo
-                    val dataString = intent.dataString
-                    val clipData = intent.clipData
-                    if (clipData != null) {
-                        results = Array(clipData.itemCount) {
-                            clipData.getItemAt(it).uri
-                        }
-                    }
-                    // Si dataString pourrait être rasé en haut du code, alors il contient l'Uri de la photo
-                    if (dataString != null) {
-                        results = arrayOf(Uri.parse(dataString))
-                    }
-                } else if (intent == null || resultCode != RESULT_OK) {
-                    // L'intention est nulle, ce qui signifie probablement que la photo a été prise avec la caméra
-                    if (mCameraPhotoPath != null) {
-                        results = arrayOf(Uri.parse(mCameraPhotoPath))
-                    }
+            val result = if (resultCode == RESULT_OK && data != null) {
+                val uri = data.data
+                if (uri != null) {
+                    arrayOf(uri)
+                } else {
+                    emptyArray()
                 }
+            } else {
+                emptyArray()
             }
-
-            mUploadMessage?.onReceiveValue(results)
-            mUploadMessage = null
+            fileUploadCallback?.onReceiveValue(result)
+            fileUploadCallback = null
         }
     }
 }
